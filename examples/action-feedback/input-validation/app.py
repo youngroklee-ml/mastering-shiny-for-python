aa8d26de8122b8dd4b319dfe642cdfc936eb27e6
Youngrok Lee <cakeil@gmail.com>
2024-01-15 10:41:11 -0500

    add input validation example

---
+++ examples/action-feedback/input-validation/app.py
@@ -0,0 +1,21 @@
+from shiny import App, ui, render, reactive, req
+from shiny_validate import InputValidator
+
+app_ui = ui.page_fluid(
+    ui.input_numeric("n", "n", value=10),
+    ui.output_text("half"),
+)
+
+def server(input, output, session):
+    iv = InputValidator()
+
+    iv.add_rule("n", lambda x: "Please select an even number" if x%2 != 0 else None)
+    iv.enable()
+
+    @render.text
+    def half():
+        even = input.n()%2 == 0
+        req(even)
+        return input.n()/2
+
+app = App(app_ui, server)
