ccec32f604eb515ee8a473753d314ca9a3490209
Youngrok Lee <cakeil@gmail.com>
2024-02-16 08:03:23 -0500

    add ui

---
+++ examples/action-transfer/cleaning-data/app.py
@@ -0,0 +1,39 @@
+from shiny import Inputs, Outputs, Session, App, reactive, render, req, ui
+
+ui_upload = ui.layout_sidebar(
+    ui.sidebar(
+        ui.input_file("file", "Data", button_label="Upload..."),
+        ui.input_text("delim", "Delimiter (leave blank to guess)", ""),
+        ui.input_numeric("skip", "Rows to skip", 0, min=0),
+        ui.input_numeric("rows", "Rows to preview", 10, min=1),
+    ),
+    ui.h3("Raw data"),
+    ui.output_table("preview1"),
+)
+
+ui_clean = ui.layout_sidebar(
+    ui.sidebar(
+        ui.input_checkbox("snake", "Rename columns to snake case?"),
+        ui.input_checkbox("constant", "Remove constant columns?"),
+        ui.input_checkbox("empty", "Remove empty cols?"),
+    ),
+    ui.h3("Cleaner data"),
+    ui.output_table("preview2"),
+)
+
+ui_download = ui.row(
+    ui.column(12, ui.download_button("download", "Download cleaner data", class_="btn-block")),
+)
+
+app_ui = ui.page_fluid(
+    ui_upload,
+    ui_clean,
+    ui_download,
+)
+
+
+def server(input: Inputs, output: Outputs, session: Session):
+    None
+
+
+app = App(app_ui, server)
